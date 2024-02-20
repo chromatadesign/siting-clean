8cce9ff0dc89bb7327ddfbcdf53e92b2c0dbb22e
Chromata <113706042+chromatadesign@users.noreply.github.com>
2024-02-20 10:46:52 -0500

    Update and rename project-page-map-1.js to project-page-map-2.js

--- project-page-map-2.js
+++ project-page-map-2.js
@@ -26,7 +26,7 @@ let map = new mapboxgl.Map({
 let mq = window.matchMedia("(min-width: 480px)");
 if (mq.matches) {
     // If the screen width is greater than 480px, set zoom level to 2
-    map.setZoom(6);
+    map.setZoom(8);
 } else {
     // If the screen width is less than 480px, set zoom level to 2
     map.setZoom(2);
