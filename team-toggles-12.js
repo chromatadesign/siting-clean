97db925548e87c2cc18fb88d8ca8f9ed43ed8966
Chromata <113706042+chromatadesign@users.noreply.github.com>
2024-03-12 12:52:26 -0400

    Update and rename team-toggles-11.js to team-toggles-12.js

---
+++ team-toggles-12.js
@@ -0,0 +1,52 @@
+
+window.$memberstackDom.getCurrentMember().then((member) => {
+    // Check if member data exists
+    if (member.data) {
+        const planConnections = member.data["planConnections"];
+        if (planConnections && planConnections.length > 0) {
+            // PLAN IDs
+            const PlanSitingClean = "pln_siting-clean-ve1f408ec";
+            const PlanAllAccess = "pln_new-all-access-i76m01m9";
+            
+            const planIds = planConnections.map(connection => connection.planId);
+            if (planIds.includes(PlanSitingClean) || planIds.includes(PlanAllAccess)) {
+                // Check if currentPlan div exists
+                var currentPlan = document.getElementById('current-plan');
+                if (currentPlan) {
+                    // Update currentPlan text based on plan ID
+                    currentPlan.innerText = planIds.includes(PlanSitingClean) ? "Siting Clean" : "All Access";
+
+                    // Adjust display of target div based on currentPlan text
+                    var targetDiv = document.getElementById(currentPlan.innerText);
+                    if (targetDiv) {
+                        targetDiv.style.display = "flex";
+                    }
+                }
+
+                // URL manipulation part
+                // Only proceed if 'currentPlan' exists to avoid errors
+                if (currentPlan) {
+                    var planText = currentPlan.innerText.toLowerCase().replace(/\s+/g, '-');
+                    var baseURL = "https://www.bantamoneportal.com/team/";
+                    var fullURL = baseURL + planText;
+                    
+                    // Check if projectsPageLinkElement exists before setting href
+                    var projectsPageLinkElement = document.getElementById('projects-page-link');
+                    if (projectsPageLinkElement) {
+                        projectsPageLinkElement.href = fullURL;
+                    }
+                }
+            } else {
+                // If currentPlan exists but no plan matches, clear its text
+                if (document.getElementById('current-plan')) {
+                    document.getElementById('current-plan').innerText = "";
+                }
+            }
+        } else {
+            console.log('no plan connections');
+        }
+    } else {
+        // Optionally, handle cases where no member data is available, e.g., redirect to login
+        // window.location.href = '/login'; // Uncomment this line if you want to redirect to login for non-members
+    }
+});
