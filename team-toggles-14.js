cd949aecc5166cc096b90d47d77eaef416ef604f
Chromata <113706042+chromatadesign@users.noreply.github.com>
2024-03-20 13:46:23 -0400

    Update and rename team-toggles-13.js to team-toggles-14.js

--- team-toggles-14.js
+++ team-toggles-14.js
@@ -1,41 +1,27 @@
-
 window.$memberstackDom.getCurrentMember().then((member) => {
     // Check if member data exists
     if (member.data) {
         const planConnections = member.data["planConnections"];
         if (planConnections && planConnections.length > 0) {
-
-            
-            // EDIT HERE ------------------------------------------
+            // ALL TEAMS HERE -------------------------------------
             const PlanSitingClean = "pln_siting-clean-ve1f408ec";
             const PlanAllAccess = "pln_new-all-access-i76m01m9";
+            const PlanBantamAccess = "pln_new-bantam-access-i700e1m9";
             // ----------------------------------------------------
 
-            
             const planIds = planConnections.map(connection => connection.planId);
             if (
-
-                
-            // EDIT HERE ------------------------------------------
-                planIds.includes(PlanSitingClean) 
-             || planIds.includes(PlanAllAccess) 
+            // ALL TEAMS HERE -------------------------------------
+                planIds.includes(PlanSitingClean) || planIds.includes(PlanAllAccess) 
             // ----------------------------------------------------
-
-                
-           ) {
+            ) {
                 // Check if currentPlan div exists
                 var currentPlan = document.getElementById('current-plan');
                 if (currentPlan) {
                     // Update currentPlan text based on plan ID
                     currentPlan.innerText = planIds.includes(PlanSitingClean) ? 
-
-                        
-             // EDIT HERE ------------------------------------------        
-                "Siting Clean" 
-              : "All Access"
-             // ----------------------------------------------------
-
-                    ;
+                    "Siting Clean" : "All Access";
+                    
                     // Adjust display of target div based on currentPlan text
                     var targetDiv = document.getElementById(currentPlan.innerText);
                     if (targetDiv) {
@@ -43,8 +29,13 @@ window.$memberstackDom.getCurrentMember().then((member) => {
                     }
                 }
 
+                // Additional functionality to set the display of "p-teams-list" to flex
+                var pTeamsList = document.getElementById("p-teams-list");
+                if (pTeamsList) {
+                    pTeamsList.style.display = "flex";
+                }
+
                 // URL manipulation part
-                // Only proceed if 'currentPlan' exists to avoid errors
                 if (currentPlan) {
                     var planText = currentPlan.innerText.toLowerCase().replace(/\s+/g, '-');
                     var baseURL = "https://www.bantamoneportal.com/partner/";
@@ -67,6 +58,6 @@ window.$memberstackDom.getCurrentMember().then((member) => {
         }
     } else {
         // Optionally, handle cases where no member data is available, e.g., redirect to login
-        
     }
 });
+
